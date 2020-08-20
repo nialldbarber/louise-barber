b44138116eb31a8eda73a3379b9479ce7e1d80ab
Niall <nialldbarber@gmail.com>
2020-08-20 19:26:51 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+// import Link from 'next/link';
+import Head from '../components/head';
+import Nav from '../components/nav';
+
+const Home: FC = () => {
+  return (
+    <div>
+      <Head title="Home" />
+      <Nav />
+
+      <div className="hero">
+        <h1 className="title">Louise Barber</h1>
+        <div className="row">
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  spaceBeforeFunctionParen: false,
+};
