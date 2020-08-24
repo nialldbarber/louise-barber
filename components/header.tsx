85721a9681472c2060f85da76cfb30f009e3c8c1
Niall <nialldbarber@gmail.com>
2020-08-24 13:38:56 +0100

    Added links to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 import Nav from 'components/nav';
 import {SITE_TITLE} from 'constants/config';
 
 const Header = () => {
   return (
     <header>
-      <h1>{SITE_TITLE}</h1>
+      <Link href="/">
+        <a>
+          <h1>{SITE_TITLE}</h1>
+        </a>
+      </Link>
       <Nav />
     </header>
   );
