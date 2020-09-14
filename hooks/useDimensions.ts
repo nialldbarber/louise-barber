4c67be36ffe17a251d4b56eb30fb5511a566dbbc
Niall <nialldbarber@gmail.com>
2020-09-14 17:20:45 +0100

    Updated hook types again

--- hooks/useDimensions.ts
+++ hooks/useDimensions.ts
@@ -11,7 +11,7 @@ interface Ref {
 
 // https://codesandbox.io/s/framer-motion-side-menu-mx2rw?fontsize=14&module=/src/Example.tsx&file=/src/use-dimensions.ts
 const useDimensions = (ref: Ref): Dimensions => {
-  const dimensions = useRef({width: 0, height: 0});
+  const dimensions = useRef<Dimensions>({width: 0, height: 0});
 
   useEffect(() => {
     dimensions.current.width = ref.current.offsetWidth;
